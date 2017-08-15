401b3b81fb9e0eb019b82d2275f30619820b75bd
tkyk <tkyk.o13@gmail.com>
2017-08-15 13:55:11 +0900

    fix

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 8000;
  
 app.get('/', function (req, res) {
   res.send('Hello World');
-})
- 
-app.listen(3000)
+});
+
+
+console.log("listen port is "+port);
+app.listen(port);
