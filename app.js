9c162c918cacb6272a8ff181cb0a7aaea6cda820
tkyk <tkyk.o13@gmail.com>
2017-08-28 11:37:29 +0900

    herokuでファイル出力可能かのテスト

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const app = express();
 // const fs = require('fs');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+var fs = require("fs");
 
 const port = process.env.PORT || 8000;
  
 app.use(express.static("./static"));
 app.get("/mess", function(req, res) {
-	console.log("query");
+	// console.log("query");
 	console.log(req.query);
 	io.emit("mess", req.query);
+
+	fs.appendFile('log.txt', req.query ,'utf8', function (err) {
+	    console.log(err);
+	});
 });
 
 
@@ -23,8 +28,6 @@ io.on('connection', function(client){
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
-
-
 
 
 
